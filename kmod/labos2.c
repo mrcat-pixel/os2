4d9da2708ebf77e456bac432b49ee9fab26ed3ad
mrcat-pixel <pixelcat@redmiso.studio>
2022-11-03 16:00:26 +0300

    attempt to get task_struct

--- kmod/labos2.c
+++ kmod/labos2.c
@@ -1,29 +1,29 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
-
 #include <linux/debugfs.h>
-
 #include <linux/pid.h>
 #include <linux/sched.h>
+#include <linux/types.h>
 
 MODULE_LICENSE("Dual BSD/GPL");
 MODULE_AUTHOR("pixelcat");
 MODULE_DESCRIPTION("labOS2 kernel module");
 MODULE_VERSION("1.0");
 
-//static struct dentry *kmod_root;
-//static struct dentry *pid1_task_struct;
-//static struct debugfs_blob_wrapper pid1_wrapper;
-//static struct task_struct *ts1;
+static struct task_struct* get_task_struct_by_pid(pid_t pid) {
+    return get_pid_task(find_get_pid(pid), PIDTYPE_PID)
+}
 
 static int __init kmod_init(void) {
-    printk(KERN_INFO "labos2: hello world\n");
+    printk(KERN_INFO "labos2: module loading\n");
+    struct task_struct* ts = get_task_struct_by_pid(1);
+    printk(KERN_INFO "labos2: test pid is %d\n", ts->pid);
     return 0;
 }
 
 static void __exit kmod_exit(void) {
-    printk(KERN_INFO "labos2: bye\n");
+    printk(KERN_INFO "labos2: module unloaded\n");
 }
 module_init(kmod_init);
 module_exit(kmod_exit);
