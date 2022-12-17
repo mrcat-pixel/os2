8f0f50d16f233d3e72122a82f75136a8fd52ccc5
Lis <mrcat.pixel@gmail.com>
2022-12-18 00:24:21 +0300

    Kernel module output to procfs

--- kmod/labos2.c
+++ kmod/labos2.c
@@ -2,56 +2,124 @@
 #include <linux/module.h>
 #include <linux/netdevice.h>
 #include <linux/kernel.h>
-#include <linux/debugfs.h>
+#include <linux/proc_fs.h>
 #include <linux/pid.h>
 #include <linux/sched.h>
+#include <linux/seq_file.h>
 #include <linux/types.h>
+#include <asm/uaccess.h>
 
-MODULE_LICENSE("Dual BSD/GPL");
-MODULE_AUTHOR("pixelcat");
-MODULE_DESCRIPTION("labOS2 kernel module");
-MODULE_VERSION("0.01");
+#define PROCFS_NAME "labOS2"
 
-static char[] nd_error_msg = "net device not found";
+static struct task_struct* ts;
+static struct net_device* nd;
+static int mode = 0;
 
 static struct task_struct* get_task_struct_by_pid(const pid_t pid) {
     struct pid* pid_instance = find_get_pid(pid);
-
     if (pid_instance) return get_pid_task(pid_instance, PIDTYPE_PID);
     else return NULL;
-}
-
-static pid_t get_pid_from_task_struct(const struct task_struct* ts) {
-    if (ts) return ts->pid;
-    else return 0;
 }
 
 static struct net_device* get_net_device_by_name(const char* name) {
     return dev_get_by_name(&init_net, name);
 }
 
-static char* get_name_from_net_device(const struct net_device* nd) {
-    if (nd) return nd->name;
-    else return &nd_error_msg;
+static int output_task_struct(struct seq_file* m, void* v) {
+    printk(KERN_INFO "labOS2: trying to display task struct info\n");
+    if (ts) {
+        seq_printf(m, "task_struct:\n");
+        seq_printf(m, "pid   = %d\n",  ts->pid);
+        seq_printf(m, "tgid  = %d\n",  ts->tgid);
+        seq_printf(m, "state = %ld\n", ts->state);
+        seq_printf(m, "flags = %d\n",  ts->flags);
+        return 0;
+    }
+    else {
+        printk(KERN_INFO "labOS2: couldn't display task struct\n");
+        seq_printf(m, "task_struct not found!\n");
+        return 0;
+    }
+}
+
+static int output_net_device(struct seq_file* m, void* v) {
+    printk(KERN_INFO "labOS2: trying to display net device info\n");
+    if (nd) {
+        seq_printf(m, "net_device:\n");
+        seq_printf(m, "name  = %s\n",  nd->name);
+        return 0;
+    }
+    else {
+        printk(KERN_INFO "labOS2: couldn't display net device\n");
+        seq_printf(m, "error getting net_device!\n");
+        return 0;
+    }
+}
+
+static int output(struct seq_file* m, void* v) {
+    switch (mode) {
+        case 1:
+            return output_task_struct(m, v);
+        case 2:
+            return output_net_device(m, v);
+        default:
+            seq_printf(m, "Input not set!\n");
+            return 0;
+    }
+}
+
+static int proc_open(struct inode* inode, struct file* file) {
+    return single_open(file, output, NULL);
+}
+
+static const struct proc_ops proc_fops = {
+    .proc_open = proc_open,
+    .proc_read = seq_read,
+    .proc_lseek = seq_lseek,
+    .proc_release = single_release,
+};
+
+static int create_proc_file(void) {
+    struct proc_dir_entry *proc_file;
+    proc_file = proc_create(PROCFS_NAME, 0, NULL, &proc_fops);
+    if (proc_file == NULL) {
+        remove_proc_entry(PROCFS_NAME, NULL);
+        return -1;
+    }
+    return 0;
 }
 
 static int __init kmod_init(void) {
-    struct task_struct* ts;
-    struct net_device* nd;
+    printk(KERN_INFO "labOS2: module loading\n");
 
-    printk(KERN_INFO "labos2: module loading\n");
+    printk(KERN_INFO "labOS2: attempting to create procfs file\n");
+    if (create_proc_file() != 0) {
+        printk(KERN_ALERT "labOS2: couldn't make /proc/%s\n", PROCFS_NAME);
+        return -ENOMEM;
+    }
+    printk(KERN_INFO "labOS2: /proc/%s created\n", PROCFS_NAME);	
+    printk(KERN_INFO "labOS2: waiting for input\n");
+
+    mode = 2;
 
     ts = get_task_struct_by_pid(1);
-    printk(KERN_INFO "labos2: test pid is %d\n", get_pid_from_task_struct(ts));
-
     nd = get_net_device_by_name("enp0s2");
-    printk(KERN_INFO "labos2: test nd name is" + get_name_from_net_device(nd) + "\n");
 
     return 0;
 }
 
 static void __exit kmod_exit(void) {
-    printk(KERN_INFO "labos2: module unloaded\n");
+    remove_proc_entry(PROCFS_NAME, NULL);
+	printk(KERN_INFO "labOS2: /proc/%s removed\n", PROCFS_NAME);
+    printk(KERN_INFO "labOS2: module unloaded\n");
 }
+
+//------------------------------------------------------------
+
+MODULE_LICENSE("Dual BSD/GPL");
+MODULE_AUTHOR("pixelcat");
+MODULE_DESCRIPTION("labOS2 kernel module");
+MODULE_VERSION("0.01");
+
 module_init(kmod_init);
 module_exit(kmod_exit);
